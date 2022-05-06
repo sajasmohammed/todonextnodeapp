1632e7223e35347dc4f222b9bb12587442f6b83c
sajasmohammed <sajasam97@gmail.com>
2022-05-06 20:51:10 +0530

    register user

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 const express=require('express');
 const dotnev=require('dotenv');
+const route=require('./routes/routes')
 app=express();
 const connectDB=require('./database/connection');
 
 dotnev.config();
-const PORT = process.env.PORT || 4000
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
 
 connectDB();
 
-app.get('/', (req, res)=>{
-    res.send("hello worlds");
-});
+app.use('/api', route);
+
 app.listen(PORT);
