6b91c85f5cbdc7160a4a4f34069bd10b6c87efc8
sajasmohammed <sajasam97@gmail.com>
2022-05-06 21:04:51 +0530

    login user

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,18 +1,34 @@
-const router=require('express').Router();
-const bcrypt=require('bcryptjs');
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const user = require('../models/user');
 const User = require('../models/user');
 
-router.post('/register', async(req, res)=>{
-    const salt=await bcrypt.genSalt(10);
-    const hashedPassword=await bcrypt.hash(req.body.password, salt);
-    const user=new User({
+router.post('/register', async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: hashedPassword
     })
-    const result=await user.save();
-    const {password, ...data}= await result.toJSON();
+    const result = await user.save();
+    const { password, ...data } = await result.toJSON();
     res.send(data);
 });
 
-module.exports=router;
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        return res.status(404).send({
+            message: "user not found"
+        })
+    }
+    if(!await bcrypt.compare(req.body.password, user.password)){
+        return res.status(404).send({
+            message:"invalid credentials"
+        });
+    }
+    res.send(user);
+})
+
+module.exports = router;
